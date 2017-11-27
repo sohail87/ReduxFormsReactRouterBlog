e7255733b6129667f27e2e6fb7c2309459221109
sohail87 <sohaildeen73@gmail.com>
2017-11-27 21:55:13 +0000

    mapping application level state to props
    wiring it up to the connect function

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -5,10 +5,11 @@ import { fetchPosts } from '../actions'
 class PostsIndex extends Component {
   //lifecycle method
   //will be called immediately after component has shown up in the DOM
-    componentDidMount() {
+  componentDidMount() {
     this.props.fetchPosts();
   }
   render() {
+    console.log(this.props.posts)
     return (
       <div>
         Posts Index
@@ -16,6 +17,11 @@ class PostsIndex extends Component {
     );
   }
 }
+//when you want to consume anything from application level state
+function mapStateToProps(state){
+  return {posts: state.posts}
+}
+
 
 //connect makes fetchPosts available in this.props for PostsIndex
-export default connect(null, {fetchPosts})(PostsIndex);
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
