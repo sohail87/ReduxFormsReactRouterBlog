49dc86f97dd49f3beec204b09b60f0a80f218a5b
sohail87 <sohaildeen73@gmail.com>
2017-12-04 22:50:16 +0000

    first field added to posts new form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import {Field, reduxForm} from 'redux-form';
+
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>
-        Posts New
-      </div>
+      <form>
+        <Field 
+          name="title"
+          component={}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+//form property must be unique!!!
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
