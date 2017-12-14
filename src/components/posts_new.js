edb3d4bb5c0b0a3a89e86dfcb41ca5fe8c273c6f
sohail87 <sohaildeen73@gmail.com>
2017-12-14 21:24:23 +0000

    comments to explain how fields work

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,30 +1,58 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+//reduxForm identical to connect 
 
+
+// <Field>  just handles data management and interaction with redux
+//  - i.e event handling and updating our different pieces of state
+// component={}  prop needs to return JSX 
+//  - that represents the acual element that is rendered on screen
+
+// To wire up the input element JSX we return in 
+//    component={this.renderTitleField}
+// to the <Field> component, so it can be responsible for 
+// handling any changes to the input element
+ 
+// we use the argument (field) which is an object
+// that has event handlers that need to be wired up to the jsx
+
+// {...field.input} is fancy jsx
+
+// it says i want all props of .input
+// to be props on the element im on, 
+// in this case the <input> element
+
+// .input is an object that contains event handlers and props
+// i.e. onChange event + value prop
 
 class PostsNew extends Component {
-  renderTitleField(field){
+  renderTitleField(field) {
     return (
       <div>
-        <input type="text"
-          {...field.input}
-        />
+        <input type="text" {...field.input}/>
       </div>
     )
   }
   render() {
     return (
       <form>
-        <Field 
-          name="title"
-          component={this.renderTitleField}
-        />
+        <Field name="title" component={this.renderTitleField}/>
       </form>
     );
   }
 }
 
-//form property must be unique!!!
-export default reduxForm({
-  form: 'PostsNewForm'
-})(PostsNew);
+//reduxForm is treated like 'connect' helper of react redux lib
+// it takes a single argument which is a function
+// the function will take some number of configuration objects
+ 
+
+//form property must be unique!!! 
+// - if 2 forms are being displayed redux can handle both if names are different
+// otherwise the form state will be merged
+export default reduxForm({form: 'PostsNewForm'})(PostsNew);
+//we use the reduxForm helper to wrap the PostsNew Component
+//by doing so we gave reduxForm the ability to communicate
+//directly from this component to the reducer we wired up before
+
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,22 @@
 import { combineReducers } from 'redux';
+
 import { reducer as formReducer } from 'redux-form';
+//recomended to alias reducer to formReducer
+
+// internally redux form uses our redux instance
+// our instance of the redux store
+// for handling all the state produced by the form
+// the form thats being rendered on the screen
+
+//redux form saving us from having to wire up action creators
+
 import PostsReducer from './reducer_posts'
+
+// the key for formReducer must be form
+
+// all the different forms that we hookup inside our different components
+// are going to assume that the formReducer is
+// being applied to the form piece of state listed below
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
