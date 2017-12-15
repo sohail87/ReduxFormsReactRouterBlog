b605f6affecd7f94298e0d15b6a1cba95dfa06bc
sohail87 <sohaildeen73@gmail.com>
2017-12-15 22:13:18 +0000

    posts reducer logic to merge the single post to our application state

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,13 +1,19 @@
 import _ from 'lodash'
-import { FETCH_POSTS } from '../actions'
+import { FETCH_POSTS, FETCH_POST } from '../actions'
 
 export default (state = {}, action) => {
   switch (action.type) {
+  case FETCH_POST:
+    //const post = action.payload.data;
+    //const newState = { ...state };
+    //newState[post.id] = post;
+    //return newState;
 
+    return { ...state, [action.payload.data.id]: action.payload.data }
+    //the return statement above is the same as the commented code above it
+    
   case FETCH_POSTS:
     return _.mapKeys(action.payload.data, 'id')
-  
-
   default:
     return state
   }
