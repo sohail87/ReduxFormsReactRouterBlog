cfeb23df99971c869d8f9ba92a240ec15833f2c6
sohail87 <sohaildeen73@gmail.com>
2017-12-15 23:06:20 +0000

    show loading message while waiting for api response and display post data.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import { fetchPost } from "../actions";
+import React, {Component} from 'react'
+import {connect} from 'react-redux';
+import {fetchPost} from "../actions";
 
 export class PostsShow extends Component {
-    componentDidMount() {
-        const { id } = this.props.match.params.id //provided to us by react router
-        this.props.fetchPost(id);
-    }
+  componentDidMount() {
+    const {id} = this.props.match.params //provided to us by react router
+    console.log(id)
+    this.props.fetchPost(id);
+  }
   render() {
+    const {post} = this.props;
+    if(!post){
+      return <div>Loading...</div>
+    }
     return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     )
   }
 }
 
 //ownProps = all the props that can be used inside component as this.props
-function mapStateToProps({posts}, ownProps) {
-  return { post: posts[ownProps.match.param.id] };
+function mapStateToProps({ posts }, ownProps) {
+  return {
+    post: posts[ownProps.match.params.id]
+  };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow)
+export default connect(mapStateToProps, {fetchPost})(PostsShow)
