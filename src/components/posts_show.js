e68a0edf75ac3cc51cc3455e74b256852e184e06
sohail87 <sohaildeen73@gmail.com>
2017-12-15 22:41:06 +0000

    posts show component calling fetchPost. map filtered state to props using ownProps

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { fetchPost } from "../actions";
 
 export class PostsShow extends Component {
+    componentDidMount() {
+        const { id } = this.props.match.params.id //provided to us by react router
+        this.props.fetchPost(id);
+    }
   render() {
     return (
       <div>
@@ -10,4 +16,9 @@ export class PostsShow extends Component {
   }
 }
 
-export default PostsShow
+//ownProps = all the props that can be used inside component as this.props
+function mapStateToProps({posts}, ownProps) {
+  return { post: posts[ownProps.match.param.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow)
